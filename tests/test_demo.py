3374d6de33d2bacd8f4ab16d6dba37d426230b3c
lee-ashworth <l.j.ashworth2185@hotmail.co.uk>
2021-02-07 13:24:45 +0000

    pytest

---
+++ tests/test_demo.py
@@ -0,0 +1,26 @@
+import unittest
+from flask import url_for
+from flask_testing import TestCase
+
+from app import app ,db , User, Car
+
+class TestBase(TestCase):
+    def create_app(self):
+        app.config.update(SQLACHEMY_DATABASE_URI="sqlite:///")
+        return app
+
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.drop_all() 
+
+class TestAccess(TestBase):
+    def test_access_home(self):
+        response = self.client.get(url_for('home'))
+        self.assertEqual(response.status_code, 200)
+        
+    
+
+
+    
